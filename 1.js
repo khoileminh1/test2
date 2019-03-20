ef2dedc2d32278ff31545cdb2a777adac07eeff1
khoileminh1 <khoileminh1@gmail.com>
2019-03-20 11:42:33 +0700

    test

---
+++ 1.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    var docao = $(window).height();
+    $('.giua1').css({ 'height': docao });
+    $(window).resize(function () {
+        var docao = $(window).height();
+        $('.giua1').css({ 'height': docao })
+    });
+});
